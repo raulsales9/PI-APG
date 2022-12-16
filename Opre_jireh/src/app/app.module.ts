91ecadd551215458ee880af0d37ac7b216a46993
Tiinocm <97448098+Tiinocm@users.noreply.github.com>
2022-12-16 08:33:10 +0100

    src: created components

--- Opre_jireh/src/app/app.module.ts
+++ Opre_jireh/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
