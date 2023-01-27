40fecc7dc613b4051443ec891b1c417d8f241b2c
ittsraul <raulsahe@protonmail.com>
2023-01-27 09:27:27 +0100

    commit views login signin

--- Opre_jireh/src/app/app.module.ts
+++ Opre_jireh/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { ContactoComponent } from './views/contacto/contacto.component';
+import { SigninComponent } from './views/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactoComponent } from './views/contacto/contacto.component';
     FooterComponent,
     HomeComponent,
     LoginComponent,
-    ContactoComponent
+    ContactoComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
