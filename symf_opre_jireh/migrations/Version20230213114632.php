98399697c50157f0081ab7959af5265ec356adb7
ittsraul <raulsahe@protonmail.com>
2023-02-13 11:49:14 +0000

    database modeled

---
+++ symf_opre_jireh/migrations/Version20230213114632.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230213114632 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorias (Id_Categoria INT AUTO_INCREMENT NOT NULL, Name_Categoria VARCHAR(50) DEFAULT NULL, PRIMARY KEY(Id_Categoria)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event (id_event INT NOT NULL, name VARCHAR(50) NOT NULL, place VARCHAR(50) DEFAULT NULL, end_date DATE NOT NULL, start_date DATE NOT NULL, description VARCHAR(150) DEFAULT NULL, PRIMARY KEY(id_event)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event_user (event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_92589AE271F7E88B (event_id), INDEX IDX_92589AE2A76ED395 (user_id), PRIMARY KEY(event_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE files (id_file INT AUTO_INCREMENT NOT NULL, id_user INT NOT NULL, name VARCHAR(50) NOT NULL, type VARCHAR(50) NOT NULL, is_submited TINYINT(1) NOT NULL, date DATE NOT NULL, INDEX IDX_63540596B3CA4B (id_user), PRIMARY KEY(id_file)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE noticias (id INT AUTO_INCREMENT NOT NULL, titulo VARCHAR(100) NOT NULL, imagen VARCHAR(255) NOT NULL, texto LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE products (id_product INT AUTO_INCREMENT NOT NULL, name_product VARCHAR(50) NOT NULL, price INT DEFAULT NULL, Id_Categoria INT NOT NULL, INDEX IDX_B3BA5A5A54E5F751 (Id_Categoria), PRIMARY KEY(id_product)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, phone VARCHAR(12) NOT NULL, name VARCHAR(50) NOT NULL, surnames VARCHAR(100) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE271F7E88B FOREIGN KEY (event_id) REFERENCES event (id_event) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE2A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE files ADD CONSTRAINT FK_63540596B3CA4B FOREIGN KEY (id_user) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5A54E5F751 FOREIGN KEY (Id_Categoria) REFERENCES categorias (Id_Categoria)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE271F7E88B');
+        $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE2A76ED395');
+        $this->addSql('ALTER TABLE files DROP FOREIGN KEY FK_63540596B3CA4B');
+        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5A54E5F751');
+        $this->addSql('DROP TABLE categorias');
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE event_user');
+        $this->addSql('DROP TABLE files');
+        $this->addSql('DROP TABLE noticias');
+        $this->addSql('DROP TABLE products');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
