e426b447df153bc79f2311f313642eaa1b7a0ed5
Natall1122 <nallca@floridauniversitaria.es>
2023-02-13 09:26:27 +0100

    routes

--- Opre_jireh/src/app/app-routing.module.ts
+++ Opre_jireh/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
