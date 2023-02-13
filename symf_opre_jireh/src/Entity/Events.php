68648e37b0af13dd46d259526252c530b6b0f023
Tino <tinoclase@gmail.com>
2023-02-13 11:10:06 +0000

    src: more changes

--- symf_opre_jireh/src/Entity/Events.php
+++ symf_opre_jireh/src/Entity/Events.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EventsRepository::class)]
-class Events
+class Event
 {
     #[ORM\Id]
     #[ORM\Column(name:'id_event')]
