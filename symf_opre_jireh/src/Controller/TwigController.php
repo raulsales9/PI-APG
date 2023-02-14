40e7ccb8f6fab7418abf14948ca89bd80cb2ef1c
ittsraul <raulsahe@protonmail.com>
2023-02-14 21:14:31 +0000

    commiting the progress, the pannel was style was made, and templates and controlers are in progress

--- symf_opre_jireh/src/Controller/TwigController.php
+++ symf_opre_jireh/src/Controller/TwigController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorias;
 use App\Entity\Event;
+use App\Entity\Products;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TwigController extends AbstractController
 {
      #[Route('/listUser/{page?}', name: 'app_panel')]
-    public function index(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    public function listUser(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
         $panel = $entityManager->getRepository(User::class)->findAll();
         $data = [];
@@ -25,10 +27,7 @@ class TwigController extends AbstractController
             "phone" => $panel[$i]->getPhone(),
             "surnames" => $panel[$i]->getSurnames()
           ];
-
-        }
-
-        
+        }        
         return $this->render('AdminPanel.html.twig', [
             'data' => $data,
             'page' => $this->getLastPage($page, $session)
@@ -45,6 +44,45 @@ class TwigController extends AbstractController
         ]);
     }
 
+    #[Route('/listProducts/{page?}', name: 'app_products')]
+    public function listProducts(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $event = $entityManager->getRepository(Products::class);
+        return $this->render('AdminProducts.html.twig', [
+            'data' => $event->findAll(),
+            "page" => $this->getLastPage($page, $session)
+        ]);
+    }
+
+    #[Route('/listCategories/{page?}', name: 'app_categories')]
+    public function listCategories(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $event = $entityManager->getRepository(Categorias::class);
+        return $this->render('AdminCategories.html.twig', [
+            'data' => $event->findAll(),
+            "page" => $this->getLastPage($page, $session)
+        ]);
+    }
+
+    #[Route('/listEvent/{page?}', name: 'app_notices')]
+    public function listNotices(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $event = $entityManager->getRepository(Noticias::class);
+        return $this->render('AdminNotices.html.twig', [
+            'data' => $event->findAll(),
+            "page" => $this->getLastPage($page, $session)
+        ]);
+    }
+
+    #[Route('/listEvent/{page?}', name: 'app_files')]
+    public function listfiles(?int $page, EntityManagerInterface $entityManager, SessionInterface $session): Response
+    {
+        $event = $entityManager->getRepository(Event::class);
+        return $this->render('AdminFiles.html.twig', [
+            'data' => $event->findAll(),
+            "page" => $this->getLastPage($page, $session)
+        ]);
+    }
             
  private function getLastPage($page, $session): int
 {
