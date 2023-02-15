dcbc8e4e9a05ddaf84ec9cae4c687f4d181bf92e
Natall1122 <nallca@floridauniversitaria.es>
2023-02-15 11:08:18 +0100

    events

--- Opre_jireh/src/app/views/eventos/eventos.interface.ts
+++ Opre_jireh/src/app/views/eventos/eventos.interface.ts
@@ -2,28 +2,38 @@ export interface Contents {
     foto: string;
     titulo: string;
     descripcion: string;
+    horario: string;
+    lugar: string;
 }
 
 export const contents: Contents[] = [
     {
         foto:  'https://i.blogs.es/2cc78a/ordenstarwars/840_560.jpg',
         titulo: 'image-container',
-        descripcion: 'Grupo Uno',
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl. Sed euismod",
+        horario: '10:00 am - 12:00 pm',
+        lugar: 'Sala 1',
     },
     {
         foto:  'https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png',
         titulo: 'image-container',
         descripcion: 'Grupo Dos',
+        horario: '10:00 am - 12:00 pm',
+        lugar: 'Sala 1',
     },
     {
         foto:  'https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png',
         titulo: 'image-container',
         descripcion: 'Grupo Dos',
+        horario: '10:00 am - 12:00 pm',
+        lugar: 'Sala 1',
     },
     {
         foto:  'https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png',
         titulo: 'image-container',
         descripcion: 'Grupo Dos',
+        horario: '10:00 am - 12:00 pm',
+        lugar: 'Sala 1',
     },
 
 ];
