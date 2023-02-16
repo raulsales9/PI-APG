3fc9d696f9261a8f07376317ed32729df678fa2a
Natall1122 <nallca@floridauniversitaria.es>
2023-02-16 13:43:42 +0100

    events api

---
+++ Opre_jireh/src/app/models/events.ts
@@ -0,0 +1,7 @@
+export interface Response {
+    name:        string;
+    place:       string;
+    end_date:    EndDateClass;
+    start_date:  EndDateClass;
+    description: string;
+}
--- Opre_jireh/src/app/services/api-request.service.ts
+++ Opre_jireh/src/app/services/api-request.service.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs";
 import { HttpClient } from '@angular/common/http'
 
 import { LoginResponse } from '../models/login-api';
+import { Response } from '../models/events';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,13 @@ export class ApiRequestService {
   constructor(public http : HttpClient) { }
 
   login = "http://localhost:8001/api/login"
+  events = "http://localhost:8000/api/events"
 
   public getLoginResponse($email : string, $password : string) : Observable<LoginResponse[]> {
     return this.http.post<LoginResponse[]>(this.login, { "email" : $email, "password" : $password })
+  }
+
+  public getEvents() : Observable<Response[]> {
+    return this.http.get<Response[]>(this.events)
   }
 }
