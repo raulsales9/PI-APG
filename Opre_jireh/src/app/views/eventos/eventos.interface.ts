f22d61310f96e668d23f1b41ce92afa4f2707e9f
Natall1122 <nallca@floridauniversitaria.es>
2023-02-16 09:46:54 +0100

    events

--- Opre_jireh/src/app/views/eventos/eventos.interface.ts
+++ Opre_jireh/src/app/views/eventos/eventos.interface.ts
@@ -40,5 +40,13 @@ export const contents: Contents[] = [
         lugar: 'Sala 1',
         id: 4,
     },
+    {
+        foto:  'https://lumiere-a.akamaihd.net/v1/images/starwars_e58d682b.png',
+        titulo: 'image-container',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel tincidunt luctus, nunc nisl aliquam massa, eget aliquam nisl nisl eu nisl.',
+        horario: '10:00 am - 12:00 pm',
+        lugar: 'Sala 1',
+        id: 4,
+    },
 
 ];
