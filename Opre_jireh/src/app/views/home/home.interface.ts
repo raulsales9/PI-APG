fe8b7c9e720e0f9ed6ee4c2252a9f8f0fde0fcfa
Tiinocm <tinoclase@gmail.com>
2023-02-19 12:48:10 +0100

    src: small change

--- Opre_jireh/src/app/views/home/home.interface.ts
+++ Opre_jireh/src/app/views/home/home.interface.ts
@@ -9,19 +9,19 @@ export const contents: News[] = [
     {
         imagen:  '../../../assets/DefNews.png',
         Titulo: 'No hay noticias o se estan cargando',
-        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpé las molestias",
+        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpe las molestias",
        
     },
     {
         imagen:  '../../../assets/DefNews.png',
         Titulo: 'No hay noticias o se estan cargando',
-        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpé las molestias",
+        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpe las molestias",
        
     },
     {
         imagen:  '../../../assets/DefNews.png',
         Titulo: 'No hay noticias o se estan cargando',
-        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpé las molestias",
+        texto: "Ahora no hay ninguna noticia o nuestros carteros se han perdido trayéndolas, disculpe las molestias",
        
     }
     
