8c232d67743f07040df716ecd82ab87edfde4669
Tiinocm <tinoclase@gmail.com>
2023-02-19 09:17:40 +0100

    src: api event problem solved

--- Opre_jireh/src/app/views/eventos/eventos.component.ts
+++ Opre_jireh/src/app/views/eventos/eventos.component.ts
@@ -18,22 +18,26 @@ export class EventosComponent {
   {
     
     this.service.getEventsResponse().subscribe(response =>{
-      console.log(response);
       for (let i = 0; i < this.contents.length; i++) {
-
+        let start_date = this.changeFormat(response[i].start_date.date);
+        let end_date =  this.changeFormat(response[i].end_date.date);
         this.contents[i] = {
           foto: "http://localhost:8000/assets/img/" + response[i].imagen,
           titulo: response[i].name,
           descripcion: response[i].description,
-          horario: response[i].start_date.date + " - " + response[i].end_date.date,
+          horario: start_date[0] + " - " + end_date,
           lugar: response[i].place,
           id: response[i].id
         };
-        console.log(contents[i]);
         
       }
 
     });
+  }
+
+  changeFormat(date : any)
+  {
+    return date.split(" ");
   }
 
 
